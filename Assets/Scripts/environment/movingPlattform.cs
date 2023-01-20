fa8ed11334ba2ecb5d5049cd6c0c6e98e8ea26af
ScorpeAKANils <90929557+ScorpeAKANils@users.noreply.github.com>
2023-01-20 13:46:57 +0100

    removed unnecessary comments, and start function

--- Assets/Scripts/environment/movingPlattform.cs
+++ Assets/Scripts/environment/movingPlattform.cs
@@ -8,16 +8,6 @@ public class movingPlattform : MonoBehaviour
     bool isOnPoint = false;
     [SerializeField] Transform[] WayPoints;
    float  speed = 15f;
-    int WayPointCounter;
- 
-    // Start is called before the first frame update
-
-    private void Start()
-    {
-        WayPointCounter = WayPoints.Length-1;
-        Debug.Log("Waypoints in der scene, -1 gezählt, weil ein Array bei 0 anfängt; " + WayPointCounter); 
-
-    }
     // Update is called once per frame
     void Update()
     {
@@ -25,7 +15,7 @@ public class movingPlattform : MonoBehaviour
         if (!isOnPoint)
         {
             transform.position = Vector3.MoveTowards(transform.position, WayPoints[index].position, speed * Time.deltaTime);
-            //wenn der gegner am ziel ist, kriege neuen weg punkt 
+           
             if (this.transform.position == WayPoints[index].position)
             {
 
