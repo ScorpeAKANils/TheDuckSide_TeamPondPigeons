a3c1df4c41e313fe1498350e53fe7962a7b727ca
ScorpeAKANils <90929557+ScorpeAKANils@users.noreply.github.com>
2023-01-23 12:55:01 +0100

    Updated MovingPlattform Script

--- Assets/ProjectAssets/Scripts/environment/movingPlattform.cs
+++ Assets/ProjectAssets/Scripts/environment/movingPlattform.cs
@@ -11,29 +11,11 @@ public class movingPlattform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (!isOnPoint)
+        if (this.transform.position == WayPoints[index].position)
         {
-            transform.position = Vector3.MoveTowards(transform.position, WayPoints[index].position, speed * Time.deltaTime);
-           
-            if (this.transform.position == WayPoints[index].position)
-            {
-
-                isOnPoint = true;
-                if (index < WayPoints.Length - 1)
-                {
-                    index++;
-                }
-                else
-                {
-                    index = 0; 
-                }
-                isOnPoint = false;
-
-            }
-
+            index++;
+            if (index == WayPoints.Length) index = 0;
         }
-    
-
+        transform.position = Vector3.MoveTowards(transform.position, WayPoints[index].position, speed * Time.deltaTime);
     }
 }
