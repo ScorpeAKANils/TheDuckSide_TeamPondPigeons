f402500d91380dc4efa69cad21633784565b2371
OnnaTheYeet <shawntheinert2016@gmail.com>
2023-01-24 12:44:50 +0100

    Fadenkreuz

--- Assets/ProjectAssets/Scripts/Gamestuff/MouseLock/MouseLock.cs
+++ Assets/ProjectAssets/Scripts/Gamestuff/MouseLock/MouseLock.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class MouseLock : MonoBehaviour
 {
-    private void Start()
-    {
-        //locked maus cursor um das bedienen der maus im spiel zu erleichtern
-        Cursor.lockState = CursorLockMode.Locked; 
-    }
+    //private void Start()
+    //{
+    //    //locked maus cursor um das bedienen der maus im spiel zu erleichtern
+    //    Cursor.lockState = CursorLockMode.Locked;
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Maus wird beim drücken von Escape entlocked -> um z.B mal in ein anderes programm zu gehen
-        
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            Cursor.lockState = CursorLockMode.None;
-        }
-        //maus wird beim drücken der linken maus taste wieder gelocked 
-        if (Input.GetButtonDown("Fire1"))
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-        
-    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    //Maus wird beim drücken von Escape entlocked -> um z.B mal in ein anderes programm zu gehen
+
+    //    if (Input.GetKeyDown(KeyCode.Escape))
+    //    {
+    //        Cursor.lockState = CursorLockMode.None;
+    //    }
+    //    //maus wird beim drücken der linken maus taste wieder gelocked 
+    //    if (Input.GetButtonDown("Fire1"))
+    //    {
+    //        Cursor.lockState = CursorLockMode.Locked;
+    //    }
+
+    //}
 }
