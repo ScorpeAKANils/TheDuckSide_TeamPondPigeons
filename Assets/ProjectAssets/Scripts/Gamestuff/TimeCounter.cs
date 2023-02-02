ec8254cc4f9663b84e0897ad8bd78c2b165e6aac
ScorpeAKANils <90929557+ScorpeAKANils@users.noreply.github.com>
2023-02-02 17:55:49 +0100

    Final LVL test

--- Assets/ProjectAssets/Scripts/Gamestuff/TimeCounter.cs
+++ Assets/ProjectAssets/Scripts/Gamestuff/TimeCounter.cs
@@ -9,6 +9,7 @@ public class TimeCounter : MonoBehaviour
     float Highscore;
     [SerializeField] TextMeshProUGUI TimeCountertxt;
     [SerializeField] TextMeshProUGUI Highscoretxt;
+ 
 
     [SerializeField] BackToMenue endLevel; 
     private void Start()
@@ -18,15 +19,23 @@ public class TimeCounter : MonoBehaviour
     }
   
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //timeCounter += Time.deltaTime;
         timeCounter = Mathf.Round((timeCounter + Time.deltaTime) * 100) / 100; 
         TimeCountertxt.text = "Time: " + timeCounter.ToString(); 
-        if (Highscore<= 0 && endLevel.EndLevel || Highscore > timeCounter && endLevel.EndLevel)
+       
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.GetComponent<BackToMenue>())
         {
-            Highscore = timeCounter;
-            PlayerPrefs.SetFloat("Highscore", Highscore); 
+            if (Highscore <= 0 && endLevel.EndLevel || Highscore > timeCounter && endLevel.EndLevel)
+            {
+                Highscore = timeCounter;
+                PlayerPrefs.SetFloat("Highscore", Highscore);
+            }
         }
     }
 }
